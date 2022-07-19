d1a6754ba57da68391d08acafadb680d552fd017
hanzhixiong <hanzhixiong@cestc.cn>
2022-07-19 16:37:38 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  title: '韩志雄的资源',
+  themeConfig: {
+    nav: [
+      { text: 'Css', link: '/Css/' },
+      { text: 'JavaScript', link: '/JavaScript/' },
+      { text: 'GitHub', link: 'https://github.com/hzxblog/web_accumulate' },
+    ],
+    sidebar: {
+      '/Css/': [
+        { 
+          title: 'CSS换行',
+          path: '/Css/wordBreak'
+        },
+      ],
+      '/JavaScript/': [
+        '',
+      ],
+    }
+  }
+}
